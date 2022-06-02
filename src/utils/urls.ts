2dd43dee05a3ef2d980fea67e0a1c18a33b1a4a7
Adeoluwa Adeboye <adeneye97@yahoo.com>
2022-06-02 14:59:38 +0100

    :construction: Configured urls for production

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,6 +1,6 @@
 const imagesUrl = () => {
 	if (process.env.NODE_ENV === "production") {
-		return "https://imageuploader.juggyprojects.com/api/v1/image";
+		return "https://unsplash.juggyprojects.com/api/v1/images";
 	} else {
 		// return "http://127.0.0.1:8000/v1/image/";
 		// return "http://localhost:8000/api/v1/images";
